d352a35f0dd7e88586bebe3eb79e69f0eb774c65
Aaron Geisler <aaron.geisler.sloth@gmail.com>
2019-06-15 14:39:28 -0700

    format code to be pretty

--- src/App.js
+++ src/App.js
@@ -5,111 +5,114 @@ import Timeline from './lib/timeline';
 import { getSampleData } from './data';
 
 const CustomTopLabel = props => {
-	return (
-		<div className="custom-top-label">
-			<p>Top Label</p>
-		</div>
-	);
+  return (
+    <div className="custom-top-label">
+      <p>Top Label</p>
+    </div>
+  );
 };
 
 const CustomBottomLabel = props => {
-	return (
-		<div className="custom-bottom-label">
-			<p>Bottom Label</p>
-		</div>
-	);
+  return (
+    <div className="custom-bottom-label">
+      <p>Bottom Label</p>
+    </div>
+  );
 };
 
 const CustomHeader = props => {
-	return (
-		<div className="custom-header">
-			<h3>Header</h3>
-		</div>
-	);
+  return (
+    <div className="custom-header">
+      <h3>Header</h3>
+    </div>
+  );
 };
 
 const CustomFooter = props => {
-	return (
-		<div className="custom-footer">
-			<h3>Footer</h3>
-		</div>
-	);
+  return (
+    <div className="custom-footer">
+      <h3>Footer</h3>
+    </div>
+  );
 };
 
 const CustomTextBody = props => {
-	return (
-		<div className="custom-text-body">
-			<h3>Text Body</h3>
-		</div>
-	);
+  return (
+    <div className="custom-text-body">
+      <h3>Text Body</h3>
+    </div>
+  );
 };
 
 const CustomImageBody = props => {
-	const { imageUrl } = props.event;
-	return (
-		<div className="custom-image-body">
-			<h3 className="image-body-label">Image Body</h3>
-			<img src={imageUrl} alt="" className="rt-image" />
-		</div>
-	);
+  const { imageUrl } = props.event;
+  return (
+    <div className="custom-image-body">
+      <h3 className="image-body-label">Image Body</h3>
+      <img src={imageUrl} alt="" className="rt-image" />
+    </div>
+  );
 };
 
 class TimelineExample extends Component {
-	static displayName = 'TimelineExample';
-	static propTypes = {};
+  static displayName = 'TimelineExample';
+  static propTypes = {};
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			events: getSampleData(),
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: getSampleData(),
       useCustomComponents: false,
-      reverseOrder: false
-		};
-	}
+      reverseOrder: false,
+    };
+  }
 
-	handleToggleUseCustomComponents(event) {
-		this.setState({ useCustomComponents: event.target.checked });
+  handleToggleUseCustomComponents(event) {
+    this.setState({ useCustomComponents: event.target.checked });
   }
   handleToggleReverseOrder(event) {
-		this.setState({ reverseOrder: event.target.checked });
-	}
+    this.setState({ reverseOrder: event.target.checked });
+  }
 
-	render() {
-		const { events, useCustomComponents, reverseOrder } = this.state;
-		const timeline = <Timeline events={events}  reverseOrder={reverseOrder}/>;
-		const customComponents = {
-			topLabel: CustomTopLabel,
-			bottomLabel: CustomBottomLabel,
-			header: CustomHeader,
-			imageBody: CustomImageBody,
-			textBody: CustomTextBody,
-			footer: CustomFooter,
-		};
-		const customTimeline = <Timeline events={events} customComponents={customComponents} reverseOrder={reverseOrder}/>;
-		return (
-			<div>
-        <div style={{textAlign:'center'}}>
+  render() {
+    const { events, useCustomComponents, reverseOrder } = this.state;
+    const timeline = <Timeline events={events} reverseOrder={reverseOrder} />;
+    const customComponents = {
+      topLabel: CustomTopLabel,
+      bottomLabel: CustomBottomLabel,
+      header: CustomHeader,
+      imageBody: CustomImageBody,
+      textBody: CustomTextBody,
+      footer: CustomFooter,
+    };
+    const customTimeline = <Timeline events={events} customComponents={customComponents} reverseOrder={reverseOrder} />;
+    return (
+      <div>
+        <div style={{ textAlign: 'center' }}>
           <h1>React Image Timeline Example</h1>
           <h4>resize window to see mobile layout</h4>
         </div>
-				<div className="toggle-container">
-					<strong>Use Custom Components</strong>
-					<input type="checkbox" onChange={this.handleToggleUseCustomComponents.bind(this)} checked={useCustomComponents} />
+        <div className="toggle-container">
+          <strong>Use Custom Components</strong>
+          <input
+            type="checkbox"
+            onChange={this.handleToggleUseCustomComponents.bind(this)}
+            checked={useCustomComponents}
+          />
           <strong>Reverse Order</strong>
-					<input type="checkbox" onChange={this.handleToggleReverseOrder.bind(this)} checked={reverseOrder} />
-					
-				</div>
-				<hr />
-				{useCustomComponents ? customTimeline : timeline}
-			</div>
-		);
-	}
+          <input type="checkbox" onChange={this.handleToggleReverseOrder.bind(this)} checked={reverseOrder} />
+        </div>
+        <hr />
+        {useCustomComponents ? customTimeline : timeline}
+      </div>
+    );
+  }
 }
 
 class App extends Component {
-	render() {
-		return <TimelineExample />;
-	}
+  render() {
+    return <TimelineExample />;
+  }
 }
 
 export default App;
--- src/lib/timeline.test.js
+++ src/lib/timeline.test.js
@@ -1,42 +1,42 @@
-import React from "react";
-import * as R from "ramda";
-import sinon from "sinon";
-import { expect } from "chai";
-import { shallow, mount, render, spy } from "enzyme";
-import Timeline from "./timeline";
+import React from 'react';
+import * as R from 'ramda';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { shallow, mount, render, spy } from 'enzyme';
+import Timeline from './timeline';
 
 const SHUFFLED_EVENTS = [
   {
-    title: "LAST",
-    imageUrl: "LAST",
-    text: "LAST",
+    title: 'LAST',
+    imageUrl: 'LAST',
+    text: 'LAST',
     date: new Date(2010, 1, 11),
-    extras: { foo: "bar" }
+    extras: { foo: 'bar' },
   },
   {
-    title: "FIRST",
-    imageUrl: "FIRST",
-    text: "FIRST",
+    title: 'FIRST',
+    imageUrl: 'FIRST',
+    text: 'FIRST',
     date: new Date(2000, 1, 11),
-    extras: { foo: "bar" }
+    extras: { foo: 'bar' },
   },
   {
-    title: "MIDDLE",
-    imageUrl: "MIDDLE",
-    text: "MIDDLE",
+    title: 'MIDDLE',
+    imageUrl: 'MIDDLE',
+    text: 'MIDDLE',
     date: new Date(2005, 1, 11),
-    extras: { foo: "bar" }
-  }
+    extras: { foo: 'bar' },
+  },
 ];
 
 const SINGLE_INVALID_EVENT = [
   {
-    title: "TITLE",
-    imageUrl: "IMAGE_URL",
-    text: "TEXT",
+    title: 'TITLE',
+    imageUrl: 'IMAGE_URL',
+    text: 'TEXT',
     date: new Date('asdfadf'),
-    extras: null
-  }
+    extras: null,
+  },
 ];
 
 const MIXED_DATES = SINGLE_INVALID_EVENT.concat(SHUFFLED_EVENTS);
@@ -67,21 +67,18 @@ const CustomBottomLabel = props => {
   return <div>*CustomBottomLabel*</div>;
 };
 
-describe("<Timeline />", () => {
-
+describe('<Timeline />', () => {
   describe('Lifecycle', () => {
-    it("mounts when rendered", () => {
+    it('mounts when rendered', () => {
       Timeline.prototype.componentDidMount = sinon.spy();
       mount(<Timeline events={[]} />);
       expect(Timeline.prototype.componentDidMount.calledOnce).to.equal(true);
     });
-  
-    it("componentWillReceiveProps is called with new props", () => {
+
+    it('componentWillReceiveProps is called with new props', () => {
       Timeline.prototype.componentWillReceiveProps = sinon.spy();
       mount(<Timeline events={[]} />).setProps({ events: [] });
-      expect(Timeline.prototype.componentWillReceiveProps.calledOnce).to.equal(
-        true
-      );
+      expect(Timeline.prototype.componentWillReceiveProps.calledOnce).to.equal(true);
     });
   });
 
@@ -89,59 +86,58 @@ describe("<Timeline />", () => {
     it("receives 'extras' in 'events' prop", () => {
       const wrapper = mount(<Timeline events={SHUFFLED_EVENTS} />);
       const { extras } = R.head(wrapper.props().events);
-      expect(extras.foo).to.equal("bar");
+      expect(extras.foo).to.equal('bar');
     });
-  
-    it("special cases 0 events", () => {
+
+    it('special cases 0 events', () => {
       expect(shallow(<Timeline events={[]} />).contains(EMPTY_DIV)).to.equal(true);
     });
 
-    it("filters events with invalid dates", () => {
+    it('filters events with invalid dates', () => {
       expect(shallow(<Timeline events={SINGLE_INVALID_EVENT} />).contains(EMPTY_DIV)).to.equal(true);
     });
 
-    it("only filters invalid dates", () => {
+    it('only filters invalid dates', () => {
       expect(shallow(<Timeline events={MIXED_DATES} />).contains(EMPTY_DIV)).to.equal(false);
     });
   });
 
   describe('Labels', () => {
-    it("renders labels on the ends of the timeline", () => {
+    it('renders labels on the ends of the timeline', () => {
       const wrapper = shallow(<Timeline events={SHUFFLED_EVENTS} />);
       expect(
         wrapper
-          .find("li")
+          .find('li')
           .first()
           .html()
       ).to.equal('<li class="rt-label-container"><div class="rt-label">2000</div></li>');
       expect(
         wrapper
-          .find("li")
+          .find('li')
           .last()
           .html()
-        ).to.equal('<li class="rt-label-container"><div class="rt-label">2010</div></li>');
+      ).to.equal('<li class="rt-label-container"><div class="rt-label">2010</div></li>');
     });
 
-    it("renders reversed labels", () => {
-      const wrapper = shallow(<Timeline events={SHUFFLED_EVENTS} reverseOrder={true}/>);
+    it('renders reversed labels', () => {
+      const wrapper = shallow(<Timeline events={SHUFFLED_EVENTS} reverseOrder={true} />);
       expect(
         wrapper
-          .find("li")
+          .find('li')
           .first()
           .html()
       ).to.equal('<li class="rt-label-container"><div class="rt-label">2010</div></li>');
       expect(
         wrapper
-          .find("li")
+          .find('li')
           .last()
           .html()
-        ).to.equal('<li class="rt-label-container"><div class="rt-label">2000</div></li>');
+      ).to.equal('<li class="rt-label-container"><div class="rt-label">2000</div></li>');
     });
   });
 
   describe('Events', () => {
-
-    it("renders events correctly", () => {
+    it('renders events correctly', () => {
       const shallowWrapper = shallow(<Timeline events={SHUFFLED_EVENTS} />);
       const deepWrapper = render(<Timeline events={SHUFFLED_EVENTS} />);
       const assertClassCountDeep = (classes, count) => {
@@ -154,111 +150,103 @@ describe("<Timeline />", () => {
           return expect(shallowWrapper.find(c)).to.have.length(count);
         }, classes);
       };
-      assertClassCountShallow([".rt-timeline", ".rt-timeline-container"], 1);
-      assertClassCountDeep([".rt-label-container"], 2);
+      assertClassCountShallow(['.rt-timeline', '.rt-timeline-container'], 1);
+      assertClassCountDeep(['.rt-label-container'], 2);
       assertClassCountDeep(
         [
-          ".rt-event",
-          ".rt-btn",
-          ".rt-image-container",
-          ".rt-text-container",
-          ".rt-header-container",
-          ".rt-footer-container"
+          '.rt-event',
+          '.rt-btn',
+          '.rt-image-container',
+          '.rt-text-container',
+          '.rt-header-container',
+          '.rt-footer-container',
         ],
         R.length(SHUFFLED_EVENTS)
       );
     });
 
-    it("events call onClick when clicked", () => {
+    it('events call onClick when clicked', () => {
       const onClick = sinon.spy();
       const spiedEvents = R.map(event => {
         return R.merge(event, { onClick });
       }, SHUFFLED_EVENTS);
       const wrapper = mount(<Timeline events={spiedEvents} />);
       wrapper
-        .find("button")
+        .find('button')
         .first()
-        .simulate("click");
+        .simulate('click');
       expect(onClick.calledOnce).to.equal(true);
     });
-  
-    it("renders events in order with unordered data", () => {
+
+    it('renders events in order with unordered data', () => {
       const wrapper = shallow(<Timeline events={SHUFFLED_EVENTS} />);
       expect(
         wrapper
-          .find(".rt-event")
+          .find('.rt-event')
           .first()
           .html()
-          .indexOf("FIRST")
+          .indexOf('FIRST')
       ).to.be.above(-1);
       expect(
         wrapper
-          .find(".rt-event")
+          .find('.rt-event')
           .last()
           .html()
-          .indexOf("LAST")
+          .indexOf('LAST')
       ).to.be.above(-1);
     });
-  
-    it("renders events in reverse-order with unordered data", () => {
-      const wrapper = shallow(
-        <Timeline events={SHUFFLED_EVENTS} reverseOrder={true} />
-      );
+
+    it('renders events in reverse-order with unordered data', () => {
+      const wrapper = shallow(<Timeline events={SHUFFLED_EVENTS} reverseOrder={true} />);
       expect(
         wrapper
-          .find(".rt-event")
+          .find('.rt-event')
           .first()
           .html()
-          .indexOf("LAST")
+          .indexOf('LAST')
       ).to.be.above(-1);
       expect(
         wrapper
-          .find(".rt-event")
+          .find('.rt-event')
           .last()
           .html()
-          .indexOf("FIRST")
+          .indexOf('FIRST')
       ).to.be.above(-1);
     });
   });
 
-  
   describe('Custom Components', () => {
-    it("renders custom components", () => {
+    it('renders custom components', () => {
       const CUSTOM_COMPONENTS = {
         topLabel: CustomTopLabel,
         bottomLabel: CustomBottomLabel,
         header: CustomHeader,
         imageBody: CustomImageBody,
         textBody: CustomTextBody,
-        footer: CustomFooter
+        footer: CustomFooter,
       };
-      const wrapper = render(
-        <Timeline
-          events={SHUFFLED_EVENTS}
-          customComponents={CUSTOM_COMPONENTS}
-        />
-      );
-  
+      const wrapper = render(<Timeline events={SHUFFLED_EVENTS} customComponents={CUSTOM_COMPONENTS} />);
+
       const firstLabelHtml = wrapper
-        .find(".rt-label-container")
+        .find('.rt-label-container')
         .first()
         .html();
-      expect(firstLabelHtml.indexOf("*CustomTopLabel*")).to.be.above(-1);
-  
+      expect(firstLabelHtml.indexOf('*CustomTopLabel*')).to.be.above(-1);
+
       const lastLabelHtml = wrapper
-        .find(".rt-label-container")
+        .find('.rt-label-container')
         .last()
         .html();
-      expect(lastLabelHtml.indexOf("*CustomBottomLabel*")).to.be.above(-1);
-  
+      expect(lastLabelHtml.indexOf('*CustomBottomLabel*')).to.be.above(-1);
+
       const eventHtml = wrapper
-        .find(".rt-event")
+        .find('.rt-event')
         .first()
         .html();
-      expect(eventHtml.indexOf("*CustomHeader*")).to.be.above(-1);
-      expect(eventHtml.indexOf("*CustomImageBody*")).to.be.above(-1);
-      expect(eventHtml.indexOf("*CustomTextBody*")).to.be.above(-1);
-      expect(eventHtml.indexOf("*CustomFooter*")).to.be.above(-1);
+      expect(eventHtml.indexOf('*CustomHeader*')).to.be.above(-1);
+      expect(eventHtml.indexOf('*CustomImageBody*')).to.be.above(-1);
+      expect(eventHtml.indexOf('*CustomTextBody*')).to.be.above(-1);
+      expect(eventHtml.indexOf('*CustomFooter*')).to.be.above(-1);
     });
   });
 });
