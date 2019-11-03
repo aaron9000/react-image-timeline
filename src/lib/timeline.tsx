feeacb2614e31c230f545fdc58070bdf08011bac
Aaron Geisler <aaron.geisler.sloth@gmail.com>
2019-11-03 07:48:51 -0800

    memoized

--- src/lib/timeline.tsx
+++ src/lib/timeline.tsx
@@ -23,15 +23,15 @@ const formattedDate = date => {
   return `${month.length > 1 ? month : '0' + month}/${day.length > 1 ? day : '0' + day}/${year}`;
 };
 
-const Dot = props => {
+const Dot = React.memo(function Dot(props) {
   return (
     <svg className="rt-dot" viewBox="0 0 8 10">
       <circle cx="4" cy="5" r="3" stroke="none" />
     </svg>
   );
-};
+});
 
-const Arrow = props => {
+const Arrow = React.memo(function Arrow(props) {
   return (
     <svg className="rt-arrow" viewBox="0 0 6 8">
       <g>
@@ -39,19 +39,19 @@ const Arrow = props => {
       </g>
     </svg>
   );
-};
+});
 
-const DefaultTopLabel = props => {
+const DefaultTopLabel = React.memo(function DefaultTopLabel(props) {
   const { event } = props;
   return <div className="rt-label">{formattedYear(event.date)}</div>;
-};
+});
 
-const DefaultBottomLabel = props => {
+const DefaultBottomLabel = React.memo(function DefaultBottomLabel(props) {
   const { event } = props;
   return <div className="rt-label">{formattedYear(event.date)}</div>;
-};
+});
 
-const DefaultHeader = props => {
+const DefaultHeader = React.memo(function DefaultHeader(props) {
   const { date, title } = props.event;
   return (
     <div>
@@ -59,9 +59,10 @@ const DefaultHeader = props => {
       <p className="rt-date">{formattedDate(date)}</p>
     </div>
   );
-};
+});
 
-const DefaultFooter = props => {
+
+const DefaultFooter = React.memo(function DefaultFooter(props) {
   const { buttonText, onClick } = props.event;
   const handleClick = e => {
     e.preventDefault();
@@ -72,119 +73,99 @@ const DefaultFooter = props => {
       {buttonText || ''}
     </button>
   );
-};
+});
 
-const DefaultTextBody = props => {
+const DefaultTextBody = React.memo(function DefaultTextBody(props) {
   const { text } = props.event;
   return (
     <div>
       <p>{text}</p>
     </div>
   );
-};
+});
 
-const DefaultImageBody = props => {
+const DefaultImageBody = React.memo(function DefaultImageBody(props) {
   const { imageUrl } = props.event;
   return (
     <div>
       <img src={imageUrl} alt="" className="rt-image" />
     </div>
   );
-};
+});
 
-const ArrowAndDot = props => {
+const ArrowAndDot = React.memo(function ArrowAndDot(props) {
   return (
     <div className="rt-svg-container">
       <Arrow />
       <Dot />
     </div>
   );
-};
+});
 
-const Clear = () => {
+const Clear = React.memo(function Clear(props) {
   return <li key="clear" className="rt-clear" />;
-};
+});
 
-class Timeline extends Component {
-  getStateForProps(props) {
-    const { events, reverseOrder } = props;
-
-    if (!isNonZeroArray(events)) {
-      return { events: [] };
-    }
-    return {
-      events: events
-        .filter(({ title, date }) => isValidDate(date))
+const Timeline = React.memo(function Timeline(props) {
+  const { events, reverseOrder, customComponents } = props;
+  const sortedEvents = events
+        .slice(0)
+        .filter(({ date }) => isValidDate(date))
         .sort((a, b) => {
           return reverseOrder ? new Date(b.date) - new Date(a.date) : new Date(a.date) - new Date(b.date);
-        }),
-    };
+        });
+
+  // Render nothing with empty events
+  if (!sortedEvents.length) {
+    return <div />;
   }
 
-  constructor(props) {
-    super(props);
-    this.state = this.getStateForProps(props);
-  }
+  // Determine which component classes to use
+  const { topLabel, bottomLabel, header, footer, imageBody, textBody } = customComponents || {};
+  const TopComponent = topLabel || DefaultTopLabel;
+  const BottomComponent = bottomLabel || DefaultBottomLabel;
+  const HeaderComponent = header || DefaultHeader;
+  const ImageBodyComponent = imageBody || DefaultImageBody;
+  const TextBodyComponent = textBody || DefaultTextBody;
+  const FooterComponent = footer || DefaultFooter;
 
-  componentWillReceiveProps(newProps) {
-    this.setState(this.getStateForProps(newProps));
-  }
-
-  render() {
-    const { events } = this.state;
-
-    // Render nothing with empty events
-    if (!events.length) {
-      return <div />;
-    }
-
-    // Determine which component classes to use
-    const { topLabel, bottomLabel, header, footer, imageBody, textBody } = this.props.customComponents || {};
-    const TopComponent = topLabel || DefaultTopLabel;
-    const BottomComponent = bottomLabel || DefaultBottomLabel;
-    const HeaderComponent = header || DefaultHeader;
-    const ImageBodyComponent = imageBody || DefaultImageBody;
-    const TextBodyComponent = textBody || DefaultTextBody;
-    const FooterComponent = footer || DefaultFooter;
-
-    return (
-      <div className="rt-timeline-container">
-        <ul className="rt-timeline">
-          <li key="top" className="rt-label-container">
-            <TopComponent event={takeFirst(events)} />
-          </li>
-          {events.map((event, index) => {
-            return (
-              <li className="rt-event" key={index}>
-                <div className="rt-backing">
-                  <ArrowAndDot />
-                  <div className="rt-content">
-                    <div className="rt-header-container">
-                      <HeaderComponent event={event} />
-                    </div>
-                    <div className="rt-image-container">
-                      <ImageBodyComponent event={event} />
-                    </div>
-                    <div className="rt-text-container">
-                      <TextBodyComponent event={event} />
-                    </div>
-                    <div className="rt-footer-container">
-                      <FooterComponent event={event} />
-                    </div>
+  return (
+    <div className="rt-timeline-container">
+      <ul className="rt-timeline">
+        <li key="top" className="rt-label-container">
+          <TopComponent event={takeFirst(sortedEvents)} />
+        </li>
+        {sortedEvents.map((event, index) => {
+          return (
+            <li className="rt-event" key={index}>
+              <div className="rt-backing">
+                <ArrowAndDot />
+                <div className="rt-content">
+                  <div className="rt-header-container">
+                    <HeaderComponent event={event} />
+                  </div>
+                  <div className="rt-image-container">
+                    <ImageBodyComponent event={event} />
+                  </div>
+                  <div className="rt-text-container">
+                    <TextBodyComponent event={event} />
+                  </div>
+                  <div className="rt-footer-container">
+                    <FooterComponent event={event} />
                   </div>
                 </div>
-              </li>
-            );
-          })}
-          <Clear />
-          <li key="bottom" className="rt-label-container">
-            <BottomComponent event={takeLast(events)} />
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+              </div>
+            </li>
+          );
+        })}
+        <Clear />
+        <li key="bottom" className="rt-label-container">
+          <BottomComponent event={takeLast(sortedEvents)} />
+        </li>
+      </ul>
+    </div>
+  );
+});
 
 Timeline.displayName = 'Timeline';
 
