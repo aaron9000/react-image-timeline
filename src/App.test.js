8ed0697e8b2ed2a588d019f5e720cd104e5badd7
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2022-11-27 16:11:49 -0500

    update dependencies and rewrite tests using react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from '@testing-library/react'
 import App from './App';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<App />);
   });
 });
